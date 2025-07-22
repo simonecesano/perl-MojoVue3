54b7bd065288abcecf776d307ec0c9ca1e0449d4
Simone Cesano <simonecesano@libero.it>
2025-07-22 06:51:45 +0200

    First commit

---
+++ components/level_two.vue
@@ -0,0 +1,13 @@
+<!-- level_two.vue -->
+<template>
+  <div>
+    <h3>Level Two Component</h3>
+    <p>This component is nested inside Level One.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LevelTwo'
+};
+</script>
