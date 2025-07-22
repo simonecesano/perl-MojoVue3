2afea1eba8c4a718b3d472d2ce925fe604eeacb4
Simone Cesano <simonecesano@libero.it>
2025-07-22 06:51:45 +0200

    First commit

---
+++ components/base.vue
@@ -0,0 +1,17 @@
+<meta route="/nested" description="this is a nested component" order="1" />
+<template>
+  <div>
+    <h1>Base Component</h1>
+    <level-one></level-one>  <!-- Nested Component -->
+  </div>
+</template>
+
+<script>
+import LevelOne from './level_one.js';
+
+export default {
+  components: {
+    LevelOne
+  }
+};
+</script>
