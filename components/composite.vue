71aca5205792de09fcdbea5e17932453fcd3161a
Simone Cesano <simonecesano@libero.it>
2025-07-22 06:51:45 +0200

    First commit

---
+++ components/composite.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <chart />
+    <woscript />
+    <wotemplate />
+  </div>
+</template>
+
+<script>
+import ChartComponent from './chart.js';
+import WoScript from './no_script.js';
+import WoTemplate from './no_template.js';
+
+export default {
+  template: template,       
+  components: {
+     chart: ChartComponent,
+     woscript: WoScript,
+     wotemplate: WoTemplate,
+  }
+};
+</script>
